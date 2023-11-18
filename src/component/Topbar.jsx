79eb8a5fdf4569cd1ccbd8b2352cc198c221ca0d
Zeeke12 <hezekiahajesi111@gmail.com>
2023-11-18 01:01:02 +0100

    Updated commit

--- src/component/Topbar.jsx
+++ src/component/Topbar.jsx
@@ -38,8 +38,11 @@ const Topbar = () => {
 
   return (
     <div className=' w-screen '>
-    <div className='bg-white fixed md:bg-[#f6f6f9] dark:bg-[#181A1E] flex items-center right-0 top-0 flex-row justify-between md:justify-end w-screen md:w-fit py-5 pr-5 dark:text-white'>
-      <button className='block md:hidden text-left' onClick={() => setOpen(!open)}><ion-icon name='menu-outline}'></ion-icon>ncncnc</button>
+    <div className='bg-white fixed md:bg-[#f6f6f9] dark:bg-[#181A1E] flex items-center right-0 top-0 flex-row justify-between md:justify-end w-screen md:w-fit py-5 px-5 dark:text-white'>
+      <button className='block md:hidden text-left' onClick={() => setOpen(!open)}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg>
+</button>
         <div className='flex flex-row justify-between gap-10 py-auto'>
             <DarkModeBtn />
             <button>Hey, Zeke</button>
@@ -47,7 +50,10 @@ const Topbar = () => {
         </div>
     </div>
     <div className={`h-screen absolute shadow-blue-700 drop-shadow-2xl shadow-2xl left-0 top-0 px-[30px] py-10 bg-white ${open ? 'block' : 'hidden'} transition-transform`}>
-        <button className='fixed top-3 right-0 ' onClick={() => setOpen(!open)}><ion-icon name='close-outline}'></ion-icon>nbnbn</button>
+        <button className='fixed top-3 right-0 ' onClick={() => setOpen(!open)}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+</svg>
+</button>
         <div className='flex flex-col  text-gray-800'>
         {sidetexts.map((sidetext, index) => <div className='text-[#7D8DA1] text-sm w-[168px] font-semibold h-[51px] px-[28px] hover:translate-x-5 hover:text-blue-700 transition-transform py-2' key={index}>{sidetext.text}</div>)}
         </div>
