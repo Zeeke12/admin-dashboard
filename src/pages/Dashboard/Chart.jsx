e81f7f18e0a44a1674559728674e97fb1b3f2773
Zeeke12 <hezekiahajesi111@gmail.com>
2023-12-23 12:34:30 +0100

    Updated Commit

--- src/pages/Dashboard/Chart.jsx
+++ src/pages/Dashboard/Chart.jsx
@@ -25,6 +25,8 @@ const Chart = () => {
             color: 'green'
         },
     ]
+
+    const textColor = 'text-[#363949] dark:text-white transition-colors duration-100'
   return (
     <div  className='flex flex-col min-[1300px]:flex-row justify-around gap-5'>
         {chartdatas.map((chartdata, index) => (<motion.div initial={{ y: 40 }} whileInView={{ y: 0 }} transition={{ duration: 1 }} key={index} className='bg-white dark:bg-[#202528] transition-colors duration-100 hover:shadow hover:transition-shadow  shadow-2xl rounded-3xl h-[200px] flex-grow flex flex-row p-5 justify-between align-middle'>
@@ -32,9 +34,9 @@ const Chart = () => {
             <span class={`material-symbols-outlined bg-${chartdata.color}-500 p-3 text-white rounded-full`} >
 {chartdata.logo}
 </span>
-<h1>{chartdata.title}</h1>
-<h1 className='font-extrabold text-2xl '>{chartdata.price}</h1>
-<h1>{chartdata.time}</h1>
+<h1 className={textColor}>{chartdata.title}</h1>
+<h1 className={`font-extrabold text-2xl ${textColor}` }>{chartdata.price}</h1>
+<h1 className={textColor}>{chartdata.time}</h1>
             </div>
             <div>
             
